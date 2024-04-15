433db2c3914a211079eb1609335b18310cc73ff8
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-04-15 09:52:14 -0400

    adding port to cors

--- python-backend/constants.py
+++ python-backend/constants.py
@@ -3,7 +3,7 @@ A centralized listing of constants that are used as settings for the API
 """
 DEFAULT_PRECISION = 30
 HOSTS = ['localhost', '127.0.0.1']
-PORTS = ['5173', '80']
+PORTS = ['5173', '80', '8000']
 # how many lines of a series to print for debugging purposes
 DEBUG_LINES = 10
 # evalf allows for verbose output via param universally set to this:
