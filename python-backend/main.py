454142c9b4516150ecbb323c9284873f026fae94
Katarzyna Rachwal <kasiarachwal@gmail.com>
2024-10-02 10:16:19 -0400

    updating node requirements and build

--- python-backend/main.py
+++ python-backend/main.py
@@ -8,7 +8,6 @@ from typing import Annotated
 import mpmath
 from fastapi import Depends, FastAPI, HTTPException, Request, status, WebSocket, WebSocketDisconnect, WebSocketException
 from fastapi.responses import JSONResponse, FileResponse, RedirectResponse
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.staticfiles import StaticFiles
 from ramanujantools import pcf
 from sympy import sympify
@@ -25,8 +24,6 @@ from wolfram_client import WolframClient
 
 sys.set_int_max_str_digits(0)
 
-security = HTTPBasic()
-
 app = FastAPI()
 
 logger = logger.config(True)
@@ -36,51 +33,23 @@ mpmath.mp.dps = constants.DEFAULT_PRECISION
 app.mount("/form", StaticFiles(directory="build"), name="react")
 
 
-def auth(creds: Annotated[HTTPBasicCredentials, Depends(security)]):
-    """
-
-    Parameters
-    ----------
-    creds : HTTPBasicCredentials username and password
-
-    Returns
-    -------
-    True if the credentials are valid, raises HTTPException with 401 Unauthorized otherwise
-    """
-    input_user = creds.username.encode("utf8")
-    correct_user = CustomSecrets.BasicUser.encode("utf8")
-    input_pass = creds.password.encode("utf8")
-    correct_pass = CustomSecrets.BasicPassword.encode("utf8")
-    if not (secrets.compare_digest(input_user, correct_user) and secrets.compare_digest(input_pass, correct_pass)):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-
-    logger.info("user authenticated")
-    return True
-
-
 @app.get("/")
-def default(authenticated=Depends(auth)) -> RedirectResponse:
+def default() -> RedirectResponse:
     """
     Make sure to redirect bare IP/url to form landing page
     """
-    if authenticated:
-        return RedirectResponse(url='/form')
+    return RedirectResponse(url='/form')
 
 
 @app.get("/form")
-def serve_frontend(authenticated=Depends(auth)) -> FileResponse:
+def serve_frontend() -> FileResponse:
     """
     Serves static React UI - facilitates embedding as iframe
     """
-    if authenticated:
-        project_path = Path(__file__).parent.resolve()
-        response = FileResponse(str(project_path / "build/index.html"), media_type="text/html")
-        response.headers["X-Frame-Options"] = "ALLOW-FROM https://www.ramanujanmachine.com"
-        return response
+    project_path = Path(__file__).parent.resolve()
+    response = FileResponse(str(project_path / "build/index.html"), media_type="text/html")
+    response.headers["X-Frame-Options"] = "ALLOW-FROM https://www.ramanujanmachine.com"
+    return response
 
 
 @app.post("/verify")
